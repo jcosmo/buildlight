6edc44767efc147fe9e981680d2e2ff3e7de5659
James Walker <github@jzwalker.id.au>
2013-01-06 23:13:21 +1100

    Experiment with using 3 different leds. Just cycles through them without any smarts.

---
+++ ardunio/src/separate_leds/separate_leds.ino
@@ -0,0 +1,22 @@
+/*
+  Experiment with 3 pins, each controlling a different colour led
+*/
+ 
+int led = 0;
+int offset = 2;
+int duration_on = 1000;
+int duration_off = 100;
+
+void setup() {                
+  pinMode(led + offset, OUTPUT);     
+  pinMode(led + offset + 1, OUTPUT);     
+  pinMode(led + offset + 2, OUTPUT);     
+}
+
+void loop() {
+  digitalWrite(led+offset, HIGH);   // current led on
+  delay(duration_on);               
+  digitalWrite(led+offset, LOW);    // current led off
+  delay(duration_off);
+  led = (led+1)%3;                  // next led
+}
